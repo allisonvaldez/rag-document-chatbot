9597ea1519193cd30db9fd4ba129f970a87e1ec2
Allison Valdez <allison.snipe@gmail.com>
2026-04-28 22:45:22 -0700

    working on app.py chunking function

--- app.py
+++ app.py
@@ -25,3 +25,35 @@ client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 chroma_client = chromadb.Client()
 # Creates a bucket for us to store document chunks
 collection = chroma_client_get_or_create_collection(name="documents")
+
+"""
+Create the PDF processing function that takes in a file as a parameter:
+    * reader = PdfReader(file): opens the PDF file to read
+    * text = "": create an empty string to store the text
+    * for page in reader.pages: create a for loop to go through each page of the document
+    * text += page.extract_text(): Takes the text off the page and add it to the string
+    * Return the collected text back to the function
+"""
+def process_pdf(file):
+    reader = PdfReader(file)
+    text = ""
+    for page in reader.pages:
+        text += page.extract_text()
+    return text
+
+"""
+Create a chunking function that takes in text and limits the chunk size to 500 as a parameters:
+    * chunk_size(500): splits the text into 500 digestible word chunks
+    * words = text.split(): put the text into indivdual words
+    * chunks = []: create an empty list to put the chunks
+    * for i in range(0, len(words), chunk_size): loop through the wordlist 500 words at a time
+    * chunk = " ".join(words[i:i + chunk_size]): get the next 500 words and join them to a string
+    * chunk.append(chunk): add the chunks to the list
+"""
+def chunk_text(text, chunk_size=500):
+    words = text.split()
+    chunks = []
+    for i in range(0, len(words), chunk_size):
+        chunk = "".join(words[i:i + chunk_size])
+        chunk.append(chuck)
+    return chunks
